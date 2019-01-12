861637d6afdfc91159427778ee682b3269f07834
Kevin Lee <123kevinlee@gmail.com>
2019-01-12 17:06:04 -0500

    Home Page Label Drag

--- HacDesktopApp/Pages/home.Designer.cs
+++ HacDesktopApp/Pages/home.Designer.cs
@@ -154,6 +154,9 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(626, 100);
             this.panel1.TabIndex = 11;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Draggable_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Draggable_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Draggable_MouseUp);
             // 
             // panel2
             // 
@@ -166,6 +169,9 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(626, 170);
             this.panel2.TabIndex = 12;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Draggable_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Draggable_MouseMove);
+            this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Draggable_MouseUp);
             // 
             // home
             // 
